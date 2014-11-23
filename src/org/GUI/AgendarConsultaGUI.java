56aeb799ae0b9ed5710b90e313eaac4dc33d0053
Jhonny de Sousa <jhonnysousa10@gmail.com>
2014-11-23 19:12:36 -0200

    Criei a janela Criar Consulta e cadastro paciente

---
+++ src/org/GUI/AgendarConsultaGUI.java
@@ -0,0 +1,145 @@
+package org.GUI;
+
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.border.BevelBorder;
+import java.awt.TextField;
+import java.awt.Button;
+
+public class AgendarConsultaGUI extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AgendarConsultaGUI frame = new AgendarConsultaGUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AgendarConsultaGUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 792, 515);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		contentPane.setLayout(new BorderLayout(0, 0));
+		setContentPane(contentPane);
+		
+		JPanel panel = new JPanel();
+		contentPane.add(panel, BorderLayout.CENTER);
+		panel.setLayout(null);
+		
+		JLabel label_3 = new JLabel("");
+		label_3.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/logo-01.png")));
+		label_3.setBounds(431, 11, 304, 138);
+		panel.add(label_3);
+		
+		JLabel lblAgendarConsulta = new JLabel("Agendar Consulta");
+		lblAgendarConsulta.setForeground(Color.WHITE);
+		lblAgendarConsulta.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 23));
+		lblAgendarConsulta.setBounds(20, 75, 217, 29);
+		panel.add(lblAgendarConsulta);
+		
+		JLabel label_2 = new JLabel("");
+		label_2.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/adicionar-01.png")));
+		label_2.setBounds(232, 75, 20, 35);
+		panel.add(label_2);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/inicial_background.png")));
+		label.setBounds(0, 0, 766, 176);
+		panel.add(label);
+		
+		JLabel lblCpfDoPaciente = new JLabel("CPF do paciente:");
+		lblCpfDoPaciente.setForeground(Color.GRAY);
+		lblCpfDoPaciente.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 18));
+		lblCpfDoPaciente.setBounds(20, 191, 148, 29);
+		panel.add(lblCpfDoPaciente);
+		
+		JPanel panel_1 = new JPanel();
+		panel_1.setBounds(10, 187, 746, 268);
+		panel.add(panel_1);
+		panel_1.setLayout(null);
+		
+		TextField textField = new TextField();
+		textField.setBounds(166, 10, 292, 22);
+		panel_1.add(textField);
+		
+		TextField textField_1 = new TextField();
+		textField_1.setBounds(10, 151, 134, 22);
+		panel_1.add(textField_1);
+		
+		TextField textField_2 = new TextField();
+		textField_2.setBounds(166, 151, 134, 22);
+		panel_1.add(textField_2);
+		
+		JLabel lblHora = new JLabel("Hora:");
+		lblHora.setForeground(Color.GRAY);
+		lblHora.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 18));
+		lblHora.setBounds(166, 119, 55, 29);
+		panel_1.add(lblHora);
+		
+		JButton btnVetificarCpf = new JButton("Vetificar CPF");
+		btnVetificarCpf.setBounds(464, 9, 134, 23);
+		panel_1.add(btnVetificarCpf);
+		
+		JLabel lblMdicos = new JLabel("M\u00E9dicos");
+		lblMdicos.setBounds(10, 53, 217, 29);
+		panel_1.add(lblMdicos);
+		lblMdicos.setForeground(Color.GRAY);
+		lblMdicos.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 18));
+		
+		JComboBox comboBox = new JComboBox();
+		comboBox.setBounds(10, 81, 128, 20);
+		panel_1.add(comboBox);
+		
+		JLabel lblData = new JLabel("Data");
+		lblData.setBounds(10, 119, 55, 29);
+		panel_1.add(lblData);
+		lblData.setForeground(Color.GRAY);
+		lblData.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 18));
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/cadastrar_paciente.png")));
+		label_1.setForeground(Color.WHITE);
+		label_1.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 23));
+		label_1.setBounds(632, 11, 104, 100);
+		panel_1.add(label_1);
+		
+		JButton button_1 = new JButton("Salvar");
+		button_1.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/salvar-01.png")));
+		button_1.setForeground(Color.WHITE);
+		button_1.setBackground(new Color(0, 128, 128));
+		button_1.setBounds(436, 214, 144, 43);
+		panel_1.add(button_1);
+		
+		JButton button_2 = new JButton("Cancelar");
+		button_2.setIcon(new ImageIcon(AgendarConsultaGUI.class.getResource("/Images/cancelar-01.png")));
+		button_2.setForeground(Color.WHITE);
+		button_2.setBackground(new Color(0, 128, 128));
+		button_2.setBounds(592, 214, 144, 43);
+		panel_1.add(button_2);
+	}
+}
--- src/org/GUI/CriarPacienteGUI.java
+++ src/org/GUI/CriarPacienteGUI.java
@@ -16,6 +16,9 @@ import javax.swing.border.EmptyBorder;
 
 import org.Classes.Arquivo;
 import org.Classes.Paciente;
+import javax.swing.ImageIcon;
+import java.awt.Font;
+import java.awt.Color;
 
 public class CriarPacienteGUI extends JDialog
 {
@@ -24,6 +27,7 @@ public class CriarPacienteGUI extends JDialog
 	private JTextField txtNome;
 	private JTextField txtCpf;
 	private JTextField txtEndereo;
+	private JTextField textField;
 
 	/**
 	 * Launch the application.
@@ -46,86 +50,89 @@ public class CriarPacienteGUI extends JDialog
 	 */
 	public CriarPacienteGUI()
 	{
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 465, 433);
 		getContentPane().setLayout(new BorderLayout());
 		contentPanel.setBorder(new EmptyBorder(5, 5, 5, 5));
 		getContentPane().add(contentPanel, BorderLayout.CENTER);
 		contentPanel.setLayout(null);
 		
-		JLabel lblNome = new JLabel("Nome");
-		lblNome.setBounds(24, 30, 66, 16);
+		JLabel lblNome = new JLabel("Nome:");
+		lblNome.setForeground(Color.GRAY);
+		lblNome.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
+		lblNome.setBounds(10, 132, 66, 16);
 		contentPanel.add(lblNome);
 		
-		JLabel lblCpf = new JLabel("CPF");
-		lblCpf.setBounds(24, 74, 66, 16);
+		JLabel lblCpf = new JLabel("CPF:");
+		lblCpf.setForeground(Color.GRAY);
+		lblCpf.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
+		lblCpf.setBounds(10, 176, 66, 16);
 		contentPanel.add(lblCpf);
 		
-		JLabel lblEndereo = new JLabel("Endereço");
-		lblEndereo.setBounds(24, 122, 66, 16);
+		JLabel lblEndereo = new JLabel("Endere\u00E7o:");
+		lblEndereo.setForeground(Color.GRAY);
+		lblEndereo.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
+		lblEndereo.setBounds(10, 224, 114, 16);
 		contentPanel.add(lblEndereo);
 		
 		txtNome = new JTextField();
-		txtNome.setBounds(108, 28, 320, 20);
+		txtNome.setBounds(114, 128, 320, 29);
 		contentPanel.add(txtNome);
 		txtNome.setColumns(10);
 		
 		txtCpf = new JTextField();
-		txtCpf.setBounds(108, 72, 320, 20);
+		txtCpf.setBounds(114, 172, 320, 29);
 		contentPanel.add(txtCpf);
 		txtCpf.setColumns(10);
 		
 		txtEndereo = new JTextField();
-		txtEndereo.setBounds(108, 120, 320, 20);
+		txtEndereo.setBounds(114, 220, 320, 29);
 		contentPanel.add(txtEndereo);
 		txtEndereo.setColumns(10);
+		
+		textField = new JTextField();
+		textField.setBounds(114, 274, 320, 29);
+		textField.setColumns(10);
+		contentPanel.add(textField);
+		
+		JPanel panel = new JPanel();
+		panel.setBounds(0, 0, 449, 109);
+		contentPanel.add(panel);
+		panel.setLayout(null);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon(CriarPacienteGUI.class.getResource("/Images/adicionar-01.png")));
+		label_1.setBounds(249, 43, 20, 35);
+		panel.add(label_1);
+		
+		JLabel lblCadastroDePaciente = new JLabel("Cadastro de Paciente");
+		lblCadastroDePaciente.setBounds(10, 43, 259, 29);
+		panel.add(lblCadastroDePaciente);
+		lblCadastroDePaciente.setForeground(Color.WHITE);
+		lblCadastroDePaciente.setFont(new Font("Helvetica65-Medium", Font.PLAIN, 23));
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon(CriarPacienteGUI.class.getResource("/Images/inicial_background.png")));
+		label.setBounds(0, 0, 449, 109);
+		panel.add(label);
+		
+		JButton button = new JButton("Salvar");
+		button.setForeground(Color.WHITE);
+		button.setBackground(new Color(0, 128, 128));
+		button.setBounds(114, 340, 144, 43);
+		contentPanel.add(button);
+		
+		JButton button_1 = new JButton("Cancelar");
+		button_1.setForeground(Color.WHITE);
+		button_1.setBackground(new Color(0, 128, 128));
+		button_1.setBounds(287, 340, 144, 43);
+		contentPanel.add(button_1);
+		
+		JLabel lblTelefone = new JLabel("Telefone:");
+		lblTelefone.setForeground(Color.GRAY);
+		lblTelefone.setFont(new Font("Arial Rounded MT Bold", Font.PLAIN, 15));
+		lblTelefone.setBounds(10, 278, 99, 16);
+		contentPanel.add(lblTelefone);
 		setTitle("Criar Paciente");
 		setModalityType(Dialog.ModalityType.APPLICATION_MODAL);
-
-		{
-			JPanel buttonPane = new JPanel();
-			buttonPane.setLayout(new FlowLayout(FlowLayout.RIGHT));
-			getContentPane().add(buttonPane, BorderLayout.SOUTH);
-			{
-				JButton okButton = new JButton("OK");
-				okButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						Paciente p = new Paciente();
-						p.setNome(txtNome.getText());
-						p.setCpf(Long.parseLong(txtCpf.getText()));
-						p.setEndereco(txtEndereo.getText());
-						
-						Arquivo.LerArquivos();
-						List<Paciente> pacientes = Arquivo.getListaPacientes();
-						
-						if(pacientes.contains(p))
-							//TODO Error!
-							;
-						else
-						{
-							//Foi adicionado o Paciente
-							
-							Arquivo.getListaPacientes().add(p);
-							Arquivo.GravarArquivo();
-							dispose();
-
-						}
-					}
-				});
-				okButton.setActionCommand("OK");
-				buttonPane.add(okButton);
-				getRootPane().setDefaultButton(okButton);
-				
-			}
-			{
-				JButton cancelButton = new JButton("Cancel");
-				cancelButton.addActionListener(new ActionListener() {
-					public void actionPerformed(ActionEvent arg0) {
-						dispose();
-					}
-				});
-				cancelButton.setActionCommand("Cancel");
-				buttonPane.add(cancelButton);
-			}
-		}
 	}
 }
